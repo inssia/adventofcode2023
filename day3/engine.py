c8615ca081b7c1b88aa2aef503a3fec231b7180a
inssia <inssia@github.com>
2023-12-10 23:53:16 -0500

    working on day3 again!

--- day3/engine.py
+++ day3/engine.py
@@ -6,6 +6,27 @@ def part1():
 
     for line in file:
         matrix.append(list(line.strip()))
+
+    ROWS, COLS = len(matrix), len(matrix[0])
+
+    running_sum = 0
+
+    def check_adjacent(r, c):
+        curr_sum = 0
+
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+        for add_r, add_c in directions:
+            pass
+            
+
+        return curr_sum
+
+    for r in range(ROWS):
+        for c in range(COLS):
+            letter = matrix[r][c]
+            if not letter.isalnum() and letter != '.':
+                running_sum += check_adjacent(r, c)
         
 
 part1()
