4e44dbdc0ad82c211d0c717ba29d335b6b65c56a
inssia <inssia@github.com>
2024-01-31 16:03:29 -0500

    day5

--- day5/almanac.py
+++ day5/almanac.py
@@ -36,7 +36,13 @@ part1()
 def part2():
     file = open("input.txt", "r").read().strip().split('\n\n')
 
-    seed_ranges = [int(x) for x in re.sub("seeds: ", "", file[0]).split()]
+    seed_ranges = []
+
+    file[0] = file[0].replace("seeds: ", "").strip().split()
+
+    for i in range(0, len(file[0]), 2):
+        seed_ranges.append([int(file[0][i]), int(file[0][i]) + int(file[0][i+1])])
+
 
     # format: [[[dest_start, source_start, step], ...], ...]
     mapped_ranges = []
@@ -51,14 +57,27 @@ def part2():
 
         mapped_ranges.append(curr_ranges)
 
-    print(find_lowest_location_from_ranges(seed_ranges, mapped_ranges))
+    print(find_lowest_location_from_seed_ranges(seed_ranges, mapped_ranges))
 
-def find_lowest_location_from_ranges(seed_ranges, mapped_ranges):
-
-    for i in range(0, len(seed_ranges), 2):
-        for map_range in mapped_ranges:
+def find_lowest_location_from_seed_ranges(seed_ranges, mapped_ranges):
     
-            pass
+    for map_range in mapped_ranges:
+        new_seed_ranges = []
+        while len(seed_ranges) > 0:
+            start, end = seed_ranges.pop(0)
+            translate_mappings(start, end, map_range, new_seed_ranges)
+
+        seed_ranges = new_seed_ranges
+
+def translate_mappings(seed_start, seed_end, map_ranges, new_seed_ranges):
+
+    print(seed_start, seed_end, new_seed_ranges, map_ranges)
+
+    for range_dest_start, range_src_start, range_step in map_ranges:
+        pass
+
+    
+    
 
 
 part2()
